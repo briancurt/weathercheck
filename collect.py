eb742437a37202ce20b20ee576f3318a7734feb0
Brian Curt <bcurtich@gmail.com>
2016-11-26 20:04:59 -0300

    Show last update time

--- collect.py
+++ collect.py
@@ -3,6 +3,9 @@
 # This little module just reads current data.
 # Values are consumed by main.
 
+import os
+import datetime
+
 def collectsmn():
     with open('/tmp/smn', 'r') as f:
         r = f.read()
@@ -17,3 +20,9 @@ def collectwc():
     with open('/tmp/wc', 'r') as f:
         r = f.read()
     return r
+
+def collecttime():
+    # Arbitrarily get time based on SMN, apply to all later.
+    val = os.path.getmtime('/tmp/smn')
+    a = datetime.datetime.fromtimestamp(val)
+    return a.strftime('%H:%M:%S')
--- display.py
+++ display.py
@@ -1,16 +1,14 @@
 #!/bin/python3
 
-from collect import collectsmn, collectaccu, collectwc
+from collect import collectsmn, collectaccu, collectwc, collecttime
 from flask import render_template
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    smn_temp = collectsmn()
-    accu_temp = collectaccu()
-    wc_temp = collectwc()
-    return render_template('index.html', smn_temp=smn_temp, accu_temp=accu_temp, wc_temp=wc_temp)
+    smn_temp, accu_temp, wc_temp, lastupdate = collectsmn(), collectaccu(), collectwc(), collecttime()
+    return render_template('index.html', smn_temp=smn_temp, accu_temp=accu_temp, wc_temp=wc_temp, lastupdate=lastupdate)
 
 if __name__ == "__main__":
     app.run('0.0.0.0')
